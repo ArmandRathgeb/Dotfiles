f03098f24fab4f53ee64bf836c239d4b46798a16
Armand Rathgeb <armand.rathgeb@gmail.com>
2021-05-31 00:24:06 -0500

    Color changes, clearer file structure, update install script some

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 BASEDIR=$PWD
+cd ~
+HOMEDIR=$PWD
+cd ${BASEDIR}
 
 pacman -S git doas gcc make zsh kitty curl neofetch go feh xfce4-screensaver dunst
 chsh -s $(which zsh)
@@ -25,13 +28,9 @@ cd ${BASEDIR}
 
 
 
-#### Shell setup ####
-ln -s ${BASEDIR}/zsh/.zshrc ~/.zshrc
 
 #### Startup session setup ####
 yay -S picom-rounded-corners
-ln -s ${BASEDIR}/desktoputils/.xinitrc ~/.xinitrc
-ln -s ${BASEDIR}/desktoputils/.fehbg ~/.fehbg
 touch ThrawnBackground.png
 # This might break if they change the link. Pray it doesn't.
 curl https://www.pixel4k.com/wp-content/uploads/2018/11/star-wars-thrawn-alliances_1541719531.jpg > ThrawnBackground.png
@@ -42,8 +41,14 @@ yay -S canta-gtk-theme
 
 
 #### Config setup ####
-ln -s ${BASEDIR}/config/kitty ~/.config/kitty
-ln -s ${BASEDIR}/config/.gtkrc-2.0 ~/.gtkrc-2.0
-ln -s ${BASEDIR}/config/gtk-3.0 ~/.config/gtk-3.0
-ln -s ${BASEDIR}/config/picom.conf ~/.config/picom.conf
+ln -s ${BASEDIR}/.config/.zshrc ${HOMEDIR}/.zshrc
+ln -s ${BASEDIR}/.config/.xinitrc ${HOMEDIR}/.xinitrc
+ln -s ${BASEDIR}/.config/.gtkrc-2.0 ${HOMEDIR}/.gtkrc-2.0
+ln -s ${BASEDIR}/.config/i3 ${HOMEDIR}/.config/i3
+ln -s ${BASEDIR}/.config/kitty ${HOMEDIR}/kitty
+ln -s ${BASEDIR}/.config/dunst ${HOMEDIR}/dunst
+ln -s ${BASEDIR}/.config/gtk-3.0 ${HOMEDIR}/.config/gtk-3.0
+ln -s ${BASEDIR}/.config/zathura ${HOMEDIR}/.config/zathura
+ln -s ${BASEDIR}/.config/i3status-rust ${HOMEDIR}/.config/i3status-rust
+ln -s ${BASEDIR}/.config/picom.conf ${HOMEDIR}/.config/picom.conf
 
